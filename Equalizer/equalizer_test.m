57d25cb7b0e3c54a4e2c5362c1e60eb8f0b0d8c9
Hyunjong Choi <noaa99@gmail.com>
2015-03-25 21:34:01 -0700

    Update equalizer_test.m

--- Equalizer/equalizer_test.m
+++ Equalizer/equalizer_test.m
@@ -13,18 +13,3 @@ input_value = [12 -6 -12 -12 -6 12 -6 12 -6];
 [ yn hn ] = Equalizer( input_value, xn, Fs );
 soundsc(yn, Fs);
 % soundsc(xn, Fs);
-
-
-
-%% FFT of sound signal
-
-% L = length(xn);       % Length of signal
-% NFFT = 2^nextpow2(L); % Next power of 2 from length of y
-% Y = fft(xn,NFFT)/L;
-% f = Fs/2*linspace(0,1,NFFT/2+1);
-% % Plot single-sided amplitude spectrum.
-% figure(400);
-% plot(f,2*abs(Y(1:NFFT/2+1))) 
-% title('Single-Sided Amplitude Spectrum of y(t)')
-% xlabel('Frequency (Hz)')
-% ylabel('|Y(f)|')
